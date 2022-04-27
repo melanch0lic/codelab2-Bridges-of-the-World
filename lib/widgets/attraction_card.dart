ff4739c27d76115af451578d32022a4a19f1e45f
melanch0lic <kudza76@mail.ru>
2022-04-27 15:11:26 +0300

    part 2 details page

--- lib/widgets/attraction_card.dart
+++ lib/widgets/attraction_card.dart
@@ -1,56 +1,63 @@
 import 'package:flutter/material.dart';
 
 import '../models/attraction.dart';
+import '../pages/details_page.dart';
 
 class AttractionCard extends StatelessWidget {
   final Color mainYellow = Color(0xFFFFB02F);
-  AttractionModel? attractionModel;
+  AttractionModel attractionModel;
 
   AttractionCard(this.attractionModel);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 180,
-      margin: EdgeInsets.all(10),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(25),
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(attractionModel!.imgPath!),
-                  fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => DetailsPage(attractionModel)));
+      },
+      child: Container(
+        width: 180,
+        margin: EdgeInsets.all(10),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(25),
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(attractionModel!.imgPath!),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Colors.transparent, Colors.black.withOpacity(0.5)],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.transparent, Colors.black.withOpacity(0.5)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(30),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(attractionModel!.name!,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold)),
-                    SizedBox(height: 10),
-                    Text(attractionModel!.location!,
-                        style: TextStyle(color: mainYellow))
-                  ]),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(attractionModel!.name!,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold)),
+                      SizedBox(height: 10),
+                      Text(attractionModel!.location!,
+                          style: TextStyle(color: mainYellow))
+                    ]),
+              )
+            ],
+          ),
         ),
       ),
     );
